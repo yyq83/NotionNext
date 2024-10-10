f5f835b5e44d36399f54adca72fee181dd91e40d
Yinqi Yang <82815012+yyq83@users.noreply.github.com>
2024-10-10 16:08:45 +0800

    Update BlogPostCard.js

--- themes/heo/components/BlogPostCard.js
+++ themes/heo/components/BlogPostCard.js
@@ -20,6 +20,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
     post?.pageCoverThumbnail &&
     !showPreview
 
+  const POST_TWO_COLS = siteConfig('HEO_HOME_POST_TWO_COLS', true, CONFIG)
   const COVER_HOVER_ENLARGE = siteConfig(
     'HEO_POST_LIST_COVER_HOVER_ENLARGE',
     true,
@@ -27,30 +28,59 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
   )
 
   return (
-    <article className={` ${COVER_HOVER_ENLARGE ? 'hover:transition-all duration-150' : ''}`}>
-      <div className='flex flex-col h-full'>
+    <article
+      className={`${COVER_HOVER_ENLARGE ? 'hover:transition-all duration-150' : ''}`}>
+      <div
+        data-wow-delay='.2s'
+        className={
+          (POST_TWO_COLS ? '2xl:h-96 2xl:flex-col' : '') +
+          ' wow fadeInUp border bg-white dark:bg-[#1e1e1e] flex mb-4 flex-col md:flex-row h-full group w-full dark:border-gray-600 hover:border-indigo-600 dark:hover:border-yellow-600 duration-300 transition-colors justify-between overflow-hidden rounded-xl'
+        }>
         {/* 图片封面 */}
         {showPageCover && (
           <Link href={post?.href} passHref legacyBehavior>
-            <div className='flex-shrink-0 overflow-hidden cursor-pointer select-none'>
+            <div
+              className={
+                (POST_TWO_COLS ? '2xl:w-full' : '') +
+                ' w-full md:w-5/12 overflow-hidden cursor-pointer select-none'
+              }>
               <LazyImage
                 priority={index === 0}
                 src={post?.pageCoverThumbnail}
                 alt={post?.title}
-                className='h-48 w-full object-cover group-hover:scale-105 group-hover:brightness-75 transition-all duration-500 ease-in-out'
+                className='h-full w-full object-cover group-hover:scale-105 group-hover:brightness-75 transition-all duration-500 ease-in-out'
               />
             </div>
           </Link>
         )}
 
         {/* 文字区块 */}
-        <div className='flex flex-col justify-between flex-grow p-4 bg-white dark:bg-[#1e1e1e]'>
+        <div
+          className={
+            (POST_TWO_COLS ? '2xl:p-4 2xl:h-48 2xl:w-full' : '') +
+            ' flex p-6 flex-col justify-between md:h-full w-full md:w-7/12'
+          }>
           <header>
+            {/* 分类 */}
+            {post?.category && (
+              <div
+                className={`flex mb-1 items-center ${showPreview ? 'justify-center' : 'justify-start'} hidden md:block flex-wrap dark:text-gray-300 text-gray-600 hover:text-indigo-700 dark:hover:text-yellow-500`}>
+                <Link
+                  passHref
+                  href={`/category/${post.category}`}
+                  className='cursor-pointer text-xs font-normal menu-link'>
+                  {post.category}
+                </Link>
+              </div>
+            )}
+
             {/* 标题和图标 */}
             <Link
               href={post?.href}
               passHref
-              className='group-hover:text-indigo-700 dark:hover:text-yellow-700 dark:group-hover:text-yellow-600 text-black dark:text-gray-100 line-clamp-2 replace cursor-pointer text-xl font-extrabold leading-tight'>
+              className={
+                'group-hover:text-indigo-700 dark:hover:text-yellow-700 dark:group-hover:text-yellow-600 text-black dark:text-gray-100 line-clamp-2 replace cursor-pointer text-xl font-extrabold leading-tight'
+              }>
               {siteConfig('POST_TITLE_ICON') && (
                 <NotionIcon
                   icon={post.pageIcon}
@@ -68,7 +98,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
             </main>
           )}
 
-          <div className='flex flex-wrap md:justify-start'>
+          <div className='md:flex-nowrap flex-wrap md:justify-start inline-block'>
             <div>
               {post.tagItems?.map(tag => (
                 <TagItemMini key={tag.name} tag={tag} />
